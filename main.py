9f640422cd04083deaa6bf8ac258e1f3b1c67eca
Dan Jakob Ofer <dan@ofer.to>
2023-06-04 17:43:46 -0400

    chore(name): from command line argument

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import sys
+
+
+def toUpper(name: str):
+    return name.capitalize()
+
+
 if __name__ == "__main__":
-    foobar = "yo"
-    hello = "hello"
-    name = "Dan"
-    print(f"{hello.upper()} {name}")
-    print(f'{"hello".upper()} {name}')
+    name = sys.argv[1]
+    print(f"Hello {toUpper(name)}")
